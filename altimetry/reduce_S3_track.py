b0817c84d163ed671bc99c0de77158ee4f5c7a7b
Bene Aschenneller <s.aschenneller@utwente.nl>
2022-04-25 21:47:07 +0200

    few corrections

--- altimetry/reduce_S3_track.py
+++ altimetry/reduce_S3_track.py
@@ -5,11 +5,11 @@ from geoslurp.datapull.ftp import Uri as ftp
 from geoslurp.datapull.ftp import Crawler
 from coastal_data import data_preparation as prep
 
-datapath_in = '~/data_temp/'
-datapath_out = '~/data_out/'
+datapath_in = '../../data_temp/'
+datapath_out = '../../data_out/'
 
 url='ftp://ftp.itc.nl/incoming/aschenneller/'
-dist2coast = 100 * 1e3; # [m]
+dist2coast = 100 * 1e3 # [m]
 
 crw = Crawler(url)
 gen = crw.ls()
@@ -28,13 +28,13 @@ for item in gen:
         geturi = ftp(url_file)
         geturi.download(datapath_in, True)
         
-        if '_enhanced' in item2[0]
-            filename_input = item2[0]
-            filename_output = item2[0].replace('_enhanced', '_mod')
-        else
+        if '_enhanced' in item2[0]:
             filename_reprocessed = item2[0]
+            filename_output = item2[0].replace('_enhanced', '_mod')
+        else:
+            filename_input = item2[0]
 
-prep.prepare_s3_data(datapath_in, filename_reprocessed, filename_input, datapath_out + filename_output, dist2coast)
+    prep.prepare_s3_data(datapath_in, datapath_out, filename_reprocessed, filename_input, filename_output, dist2coast)
 
 #end_time = time.process_time()
 #ex_time = end_time - start_time
