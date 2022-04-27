ddf94f02be6e91da00ffbc35111260349aeecf83
Susann Aschenneller <aschennellers@linux373.itc.utwente.nl>
2022-04-27 17:25:58 +0200

    fixed merge mistake

--- altimetry/reduce_S3_track.py
+++ altimetry/reduce_S3_track.py
@@ -1,21 +1,15 @@
 #import time
 #start_time = time.process_time()
-<<<<<<< HEAD
 
 from geoslurp.datapull.ftp import Uri as ftp
 from geoslurp.datapull.ftp import Crawler
 from geoslurp.db.settings import Credentials
 from geoslurp.config import setInfoLevel
 setInfoLevel()
-=======
->>>>>>> b0817c84d163ed671bc99c0de77158ee4f5c7a7b
 
-from geoslurp.datapull.ftp import Uri as ftp
-from geoslurp.datapull.ftp import Crawler
 from coastal_data import data_preparation as prep
 
 datapath_in = '../../data_temp/'
-<<<<<<< HEAD
 datapath_out = '~/data_out/'
 
 url='ftp://ftp.itc.nl/incoming/aschenneller/'
@@ -52,38 +46,3 @@ for item in gen:
 #ex_time = end_time - start_time
 #rint("Done. Needed ", ex_time, "seconds.")
 
-=======
-datapath_out = '../../data_out/'
-
-url='ftp://ftp.itc.nl/incoming/aschenneller/'
-dist2coast = 100 * 1e3 # [m]
-
-crw = Crawler(url)
-gen = crw.ls()
-
-for item in gen:
-    filename_input = ''
-    filename_output = ''
-    filename_reprocessed = ''
-    
-    url_temp = url + item[0] + 'processing/'
-    
-    crw2 = Crawler(url_temp)
-    gen2 = crw2.ls()
-    for item2 in gen2:
-        url_file = url_temp + item2[0]
-        geturi = ftp(url_file)
-        geturi.download(datapath_in, True)
-        
-        if '_enhanced' in item2[0]:
-            filename_reprocessed = item2[0]
-            filename_output = item2[0].replace('_enhanced', '_mod')
-        else:
-            filename_input = item2[0]
-
-    prep.prepare_s3_data(datapath_in, datapath_out, filename_reprocessed, filename_input, filename_output, dist2coast)
-
-#end_time = time.process_time()
-#ex_time = end_time - start_time
-#rint("Done. Needed ", ex_time, "seconds.")
->>>>>>> b0817c84d163ed671bc99c0de77158ee4f5c7a7b
