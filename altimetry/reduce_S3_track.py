073a908e2526100c7afd6a66ee718c02fed41a02
Susann Aschenneller <s.aschenneller@utwente.nl>
2022-04-28 12:12:12 +0200

    works now also for several tracks in one folder (finding corresponding input and reprocessed files

--- altimetry/reduce_S3_track.py
+++ altimetry/reduce_S3_track.py
@@ -1,6 +1,3 @@
-#import time
-#start_time = time.process_time()
-
 from geoslurp.datapull.ftp import Uri as ftp
 from geoslurp.datapull.ftp import Crawler
 from geoslurp.db.settings import Credentials
@@ -10,41 +7,46 @@ setInfoLevel()
 from coastal_data import data_preparation as prep
 import os
 
+#datapath_in = '../../data_temp/'
+#datapath_out = '/home/aschennellers/data_out/'
 datapath_in = '../../data_temp/'
-datapath_out = '/home/aschennellers/data_out/'
+datapath_out = '../../data_out/'
 
 url='ftp://ftp.itc.nl/incoming/aschenneller/'
 dist2coast = 100 * 1e3 # [m]
 
 auth=Credentials(alias="ftpitc",user="ftpguest",passw="ftp4you",url=url,ftptls=True)
 
-crw = Crawler(auth.url,auth=auth,pattern="S3B.*nc")
-gen = crw.ls()
+crw = Crawler(auth.url,auth=auth) # 1st crawler to get the name of the folder (1 folder = 1 EarthConsole task)
+folder_crawler = crw.ls()
 
-for item in gen:
-    filename_input = ''
-    filename_output = ''
-    filename_reprocessed = ''
+for folder in folder_crawler:
+    url_temp = url + folder[0] + 'processing/'
     
-    url_temp = url + item[0] + 'processing/'
+    #crw2 = Crawler(url_temp, auth=auth, pattern="*xy*") # 2nd crawler to get the filenames # giving a pattern does not change the output?
+    crw2 = Crawler(url_temp, auth=auth)
+    files = list(crw2.ls())
     
-    crw2 = Crawler(url_temp)
-    gen2 = crw2.ls()
-    for item2 in gen2:
-        url_file = url_temp + item2[0]
-        geturi = ftp(url_file)
-        geturi.download(datapath_in, True)
-        
-        if '_enhanced' in item2[0]:
-            filename_reprocessed = item2[0]
-            filename_output = item2[0].replace('_enhanced', '_mod')
-        else:
-            filename_input = item2[0]
+    related_filenames = [] # list of tuples, each tuple contains the filenames of the two corresponding .nc-files, 1st element: input file, 2nd element: reprocessed file
 
-    prep.prepare_s3_data(datapath_in, datapath_out, filename_reprocessed, filename_input, filename_output, dist2coast)
-    
-    os.system("smbclient -A ~/.smbclient.conf //ad.utwente.nl/ITC/WRS/Group -D 2_Research/PhDs/Aschenneller/Sentinel-3B/ -c 'lcd " + datapath_out + "; put " + filename_output + "'")
-#end_time = time.process_time()
-#ex_time = end_time - start_time
-#rint("Done. Needed ", ex_time, "seconds.")
+    for i in range(0, len(files)):
+        if '_enhanced' in files[i][0]:
+            repr_fname_temp = files[i][0]
+            orig_filename_temp = repr_fname_temp.replace('_enhanced', '')
+            related_filenames.append((orig_filename_temp, repr_fname_temp))
+            
+    for i in range(0, len(related_filenames)):
+        filename_input = related_filenames[i][0]
+        filename_reprocessed = related_filenames[i][1]
+        filename_output = filename_reprocessed.replace('_enhanced', '_mod')
 
+        geturi_in = ftp(url_temp + filename_input)
+        geturi_re = ftp(url_temp + filename_reprocessed)
+
+        geturi_in.download(datapath_in, True)
+        geturi_re.download(datapath_in, True)
+
+        prep.prepare_s3_data(datapath_in, datapath_out, filename_reprocessed, filename_input, filename_output, dist2coast)
+    
+    #os.system("smbclient -A ~/.smbclient.conf //ad.utwente.nl/ITC/WRS/Group -D 2_Research/PhDs/Aschenneller/Sentinel-3B/ -c 'lcd " + datapath_out + "; put " + filename_output + "'")
+    #os.system("rm " + datapath_in + "/* " + datapath_out + "/*")
